6559aef4f7f876ad08e5928a94085885fae4ad36
Luiz Miguel <miguelcomputerengineer@gmail.com>
2026-03-16 06:16:25 -0300

    Refactor end of program to use halt instruction
    Replaced 'add zero,zero,zero' with 'halt' to end the program.

--- laboratório2/atividade3/atividade3 - Luiz Miguel.asm
+++ laboratório2/atividade3/atividade3 - Luiz Miguel.asm
@@ -9,19 +9,12 @@ beq x22,x23,soma
 sub x19,x20,x21
 beq zero,zero,fim
 
-soma:
-add x19,x20,x21
+soma: add x19,x20,x21
 
-fim:
-add zero,zero,zero
+fim: halt
 
 f: .word 0x0000
 g: .word 0x1
 h: .word 0x1
 i: .word 0x0000
 j: .word 0x0000
-
-
-
-
-
