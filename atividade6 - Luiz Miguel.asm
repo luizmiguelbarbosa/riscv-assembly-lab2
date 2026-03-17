9e73ae58d0aff06a3a9b66a19c97c96b630457f1
Luiz Miguel <miguelcomputerengineer@gmail.com>
2026-03-17 07:47:13 -0300

    Add files via upload

---
+++ atividade6 - Luiz Miguel.asm
@@ -0,0 +1,27 @@
+addi x20,zero,4
+addi x21,zero,128
+lb x20,1029(x0)
+
+esperar_apertar:
+lb x10,1026(x0)
+andi x10,x10,0x1
+beq x10,x0,esperar_apertar
+
+esperar_soltar:
+lb x10,1026(x0)
+andi x10,x10,0x1
+bne x10,x0,esperar_soltar
+
+loop:
+slli x20,x20,1 
+lb x20,1029(x0)
+beq x20,x21,saida
+bne x20,x21,loop 
+ 
+saida: halt 
+
+
+
+
+
+
