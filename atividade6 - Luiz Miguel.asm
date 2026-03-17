f7d0432ef2df259194e080da3dc204814ddde839
Luiz Miguel <miguelcomputerengineer@gmail.com>
2026-03-17 07:54:47 -0300

    Change lb to sb for storing byte in memory

--- atividade6 - Luiz Miguel.asm
+++ atividade6 - Luiz Miguel.asm
@@ -1,6 +1,6 @@
 addi x20,zero,4
 addi x21,zero,128
-lb x20,1029(x0)
+sb x20,1029(x0)
 
 esperar_apertar:
 lb x10,1026(x0)
@@ -12,16 +12,11 @@ lb x10,1026(x0)
 andi x10,x10,0x1
 bne x10,x0,esperar_soltar
 
-loop:
 slli x20,x20,1 
-lb x20,1029(x0)
+sb x20,1029(x0)
 beq x20,x21,saida
-bne x20,x21,loop 
+
+jal x0,esperar_apertar 
  
 saida: halt 
-
-
-
-
-
 
