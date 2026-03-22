e88a1632232e4ec40bf16f067251c6a38f35b1ce
Luiz Miguel <miguelcomputerengineer@gmail.com>
2026-03-22 13:18:04 -0300

    Add files via upload

---
+++ activity6.asm
@@ -0,0 +1,23 @@
+addi x20, zero, 1
+addi x21, zero, 128
+sb x20, 1029(x0)
+
+esperar_apertar:
+lb x10, 1028(x0)
+andi x10, x10, 7
+beq x10, x0, esperar_apertar
+
+esperar_soltar:
+lb x10, 1028(x0)
+andi x10, x10, 7
+bne x10, x0, esperar_soltar
+
+slli x20, x20, 1
+sb x20, 1029(x0)
+
+beq x20, x21, saida
+jal x0, esperar_apertar
+
+saida:
+halt
+
